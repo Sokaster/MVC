4854240c47a3aba03339cc82b419e146e16533f2
Sokaster <danenkov.dmitriy@list.ru>
2022-09-01 19:57:20 +0300

    da

---
+++ WebMVCHomeTask/Controllers/BookController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMVCHomeTask.Models;
+namespace WebMVCHomeTask.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            BookViewModel book = new BookViewModel("Stiven King","ONO", 342);
+
+            return View(book);
+
+        }
+    }
+}
---
+++ WebMVCHomeTask/Models/BookViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace WebMVCHomeTask.Models
+{
+    public class BookViewModel
+    {
+        public BookViewModel(string author, string nameOfBook, int countOfPages)
+        {
+            Author = author;
+            NameOfBook = nameOfBook;
+            CountOfPages = countOfPages;
+        }
+
+        public string Author { get; set; }
+        public string NameOfBook { get; set; }
+        public int CountOfPages { get; set; }
+    }
+}
